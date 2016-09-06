eae91b8cae98e9871bc2c8cad05dee7994d4ccc6
Angus <angustalves@gmail.com>
2016-09-06 03:14:05 +0000

    initial dev build

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
-
+var lp = process.env.PORT || 8080;
 app.get('/', function (req, res) {
-  res.send('<h1>QRFDEV Timestamp Microservice</h1>');
+    res.send('<h1>QRFDEV Timestamp Microservice</h1>');
 });
 
-app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port '+process.env.PORT+'!');
+app.get(/\/(.*)/, function (req, res) {
+    var date;
+    var retobj = {};
+    var monthNames = [ "January", "February", "March","April", "May", "June", "July"
+                        ,"August", "September", "October","November", "December"
+    ];
+    try{
+        console.log(req.params);
+        date = Date.parse('2016 10 31');
+        retobj.unix = date.getTime();
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+        console.log(monthNames[monthIndex], day, year);
+        retobj.natural = monthNames[monthIndex]+' '+day+', '+year;
+    }catch(err){
+        
+    }
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.send(JSON.stringify(retobj));
+});
+
+app.listen(lp, function () {
+    console.log('Example app listening on port '+lp+'!');
 });
